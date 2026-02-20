4906e71f06445cb81d47f52969834cf375e68507
Shivam Tiwari <shivamt2012@gmail.com>
2026-02-21 01:37:50 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('CI/CD Working!'));
+app.listen(3000, () => console.log('App running on port 3000'));
